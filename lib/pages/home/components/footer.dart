dc1a34b74b38fdb118feab914303f5717094ac1c
Javier Benitez <javier.benitezf51@gmail.com>
2022-02-26 18:48:19 +0000

    added new footer

--- lib/pages/home/components/footer.dart
+++ lib/pages/home/components/footer.dart
@@ -3,17 +3,44 @@ import 'package:http/http.dart' as http;
 
 import 'package:flutter/material.dart';
 import 'package:footer/footer.dart' as footerlib;
+import 'package:url_launcher/url_launcher.dart';
 
 class Footer extends StatelessWidget {
   const Footer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return footerlib.Footer(
-      child:
-          const Text('Original Portfolio Designed & Built by Javier Benitez'),
-      padding: const EdgeInsets.all(10.0),
-      backgroundColor: Colors.transparent,
+    return FutureBuilder<int>(
+      future: getRepoCounts(), // a Future<String> or null
+      builder: (BuildContext context, AsyncSnapshot<int> result) {
+        switch (result.connectionState) {
+          case ConnectionState.none:
+            return Container();
+          case ConnectionState.waiting:
+            return Container();
+          default:
+            if (result.hasError) {
+              return Container();
+            } else {
+              return footerlib.Footer(
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      const Text(
+                          'Original Portfolio Designed & Built by Javier Benitez'),
+                      Text('Github Stars ${result.data}',
+                          textAlign: TextAlign.center),
+                    ],
+                  ),
+                  onTap: () => launch(
+                      'https://github.com/javib51/flutter-portfolio'),
+                ),
+                padding: const EdgeInsets.all(10.0),
+                backgroundColor: Colors.transparent,
+              );
+            }
+        }
+      },
     );
   }
 
@@ -21,8 +48,8 @@ class Footer extends StatelessWidget {
     int count = 0;
     var client = http.Client();
     try {
-      var response = await client.get(
-          Uri.https('api.github.com', 'repos/javib51/flutter-portfolio'),);
+      var response = await client
+          .get(Uri.https('api.github.com', 'repos/javib51/flutter-portfolio'));
       var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
       count = decodedResponse["stargazers_count"];
     } finally {
